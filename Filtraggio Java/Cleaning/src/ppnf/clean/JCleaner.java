62e24a685c5e36bc4e1a4e827a9cc8d2143a2687
Nicola Antoniazzi <nicola.antoniazzi@gmail.com>
2015-05-28 10:19:55 +0200

    implementato inserimento nella tabella clean_tweet

--- Filtraggio Java/Cleaning/src/ppnf/clean/JCleaner.java
+++ Filtraggio Java/Cleaning/src/ppnf/clean/JCleaner.java
@@ -1,4 +1,5 @@
 package ppnf.clean;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,56 +10,70 @@ import ppnf.conn.JConnection;
 
 public class JCleaner {
 
-	
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		Connection connection = JConnection.connect();
-		
+
 		try {
-			
+
 			connection.setAutoCommit(false);
-			
+
 			Statement cmdTrashTweet = connection.createStatement();
 			String queryTrashTweet = "SELECT id, text FROM trash_tweet";
 			ResultSet rsTrashTweet = cmdTrashTweet.executeQuery(queryTrashTweet);
-			
-			Statement cmdBlackList = connection.createStatement();
+
+			Statement cmdBlackList = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			String queryBlackList = "SELECT word FROM blacklist";
 			ResultSet rsBlackList = cmdBlackList.executeQuery(queryBlackList);
-			
-			Statement cmdWhiteList = connection.createStatement();
+
+			Statement cmdWhiteList = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			String queryWhiteList = "SELECT word FROM whitelist";
 			ResultSet rsWhiteList = cmdWhiteList.executeQuery(queryWhiteList);
-			
-			PreparedStatement cmdCleanTweet = connection.prepareStatement("INSERT INTO clean_tweet(id_trash) VALUES (?)");
-			
-			boolean whiteList = false;
-			boolean blackList = false;
-			
-			while(rsTrashTweet.next()){
-				String text = rsTrashTweet.getString("text");
-				while(rsBlackList.next()){
+
+			String preparedInsClean = "INSERT INTO clean_tweet(id_trash) VALUES (?)";
+			PreparedStatement cmdCleanTweet = connection.prepareStatement(preparedInsClean);
+
+			boolean whiteList;
+			boolean blackList;
+			int tmp = 0; //remove IT !!!
+			while (rsTrashTweet.next()) {
+
+				whiteList = false;
+				blackList = false;
+
+				String text = rsTrashTweet.getString("text").toLowerCase();
+				
+				while (rsBlackList.next()) {
 					String word = rsBlackList.getString("word");
-					if(text.contains(word)){
-						blackList=true;
-					}
+					if (text.contains(word)) {
+						blackList = true;
+					}					
 				}
-				while(rsWhiteList.next()){
+				rsBlackList.first();
+				
+				while (rsWhiteList.next()) {
 					String word = rsWhiteList.getString("word");
-					if(text.contains(word)){
-						whiteList=true;
+					if (text.contains(word)) {
+						whiteList = true;
 					}
+					
 				}
-				if(whiteList && !blackList){
-					cmdCleanTweet.setInt(2, rsTrashTweet.getInt("id"));
-				}
+				rsWhiteList.first();
+				
+				if (whiteList && !blackList) {
+					//cmdCleanTweet.setInt(1, rsTrashTweet.getInt("id"));
+					//cmdCleanTweet.executeUpdate();
+					System.out.println(text);
+					tmp++;
+					
+				}					
 			}
-			
+			System.out.println(tmp);
 			connection.commit();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 }
