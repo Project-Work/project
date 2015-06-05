a6a1a9709adecd115a4258f67396ee98ef26c284
Nicola Antoniazzi <nicola.antoniazzi@gmail.com>
2015-06-05 09:00:34 +0200

    Last minute

--- python/save_state.py
+++ python/save_state.py
@@ -18,26 +18,23 @@ def get_bearer_token(c, a):
 
 
 def save_state(state):
+    import os
     import psycopg2
     import requests
-    token = get_bearer_token('eI73Ec9V7eBBctSMTQ4CmiYm6', 'KcrqXsaxFNXbxtqUWh1FROxE3tZUQhKegvZKiKj3jO42j4gh2r')
+    token = get_bearer_token(os.environ['APP_KEY'], os.environ['APP_SECRET'])
     url = 'https://api.twitter.com/1.1/geo/search.json'
     resp = requests.get(
-        url, 
+        url,
         params={'granularity':'country', 'query': state},
         headers={'Authorization': 'Bearer {}'.format(token)}
         )
     data = resp.json()
     id = data['result']['places'][0]['id']
-    conn = psycopg2.connect(user = 'projectwork', password= 'projectwork' , dbname= 'projectwork', host= '192.168.101.208' , port = 5432 )
+    conn = psycopg2.connect(user = 'projectwork', password= 'password' , dbname= 'projectwork', host= '127.0.0.1' , port = 5432 )
     curs = conn.cursor()
-    curs.execute('INSERT INTO state VALUES (%s, %s)',
-        (id, state)    
+    curs.execute('INSERT INTO countries VALUES (%s, %s)',
+        (id, state)
     )
     conn.commit()
     curs.close()
     conn.close()
-
-        
-
-    
